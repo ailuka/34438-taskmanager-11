101d6a9c98bd2cdc4eab4c34881478654737fa8d
Yulia Karpova <karpova.jul@gmail.com>
2020-04-10 13:30:50 +1000

    Создает функцию для генерации моков с задачами.

---
+++ src/mock/task.js
@@ -0,0 +1,72 @@
+import {COLORS} from "../const.js";
+
+const TaskDescriptions = [
+  `Изучить теорию`,
+  `Сделать домашку`,
+  `Пройти интенсив на соточку`
+];
+
+const DefaultRepeatingDays = {
+  mo: false,
+  tu: false,
+  we: false,
+  th: false,
+  fr: false,
+  st: false,
+  sn: false,
+};
+
+const getRandomArrayItem = (array) => {
+  const randomIndex = getRandomIntegerNumber(0, array.length);
+
+  return array[randomIndex];
+};
+
+const getRandomIntegerNumber = (min, max) => {
+  return min + Math.floor(Math.random() * (max - min));
+};
+
+// Ограничение: плюс-минус неделя от текущей даты.
+const getRandomDate = () => {
+  const targetDate = new Date();
+  const sign = Math.random() > 0.5 ? 1 : -1;
+  const diffValue = sign * getRandomIntegerNumber(0, 8);
+
+  targetDate.setDate(targetDate.getDate() + diffValue);
+
+  return targetDate;
+};
+
+// repeatingDays - объект с фиксированными ключами mo, tu, we, th, fr, sa, su и булевым значением.
+// Повторяется ли задача будем узнавать именно из него;
+const generateRepeatingDays = () => {
+  return Object.assign({}, DefaultRepeatingDays, {
+    mo: Math.random() > 0.5,
+    tu: Math.random() > 0.5,
+    we: Math.random() > 0.5,
+    th: Math.random() > 0.5,
+    fr: Math.random() > 0.5,
+    st: Math.random() > 0.5,
+    sn: Math.random() > 0.5,
+  });
+};
+
+const generateTask = () => {
+  // dueDate - Объект типа Date или null, если срок исполнения не установлен.
+  const dueDate = Math.random() > 0.5 ? null : getRandomDate();
+
+  return {
+    description: getRandomArrayItem(TaskDescriptions),
+    dueDate,
+    repeatingDays: dueDate ? DefaultRepeatingDays : generateRepeatingDays(),
+    color: getRandomArrayItem(COLORS),
+    isFavorite: Math.random() > 0.5,
+    isArchive: Math.random() > 0.5,
+  };
+};
+
+const generateTasks = (count) => {
+  return new Array(count).fill(``).map(generateTask);
+};
+
+export {generateTask, generateTasks};
