1fd1d7d3755ec9ba7d083f48309ca8d483c17e75
Yulia Karpova <karpova.jul@gmail.com>
2020-04-18 17:22:08 +1000

    Создает и применяет функцию replace для замены одного DOM-элемента на другой

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import SortComponent from "./components/sort.js";
 import NoTasksComponent from "./components/no-tasks.js";
 import {generateTasks} from "./mock/task.js";
 import {generateFilters} from "./mock/filter.js";
-import {render, RenderPosition} from "./utils/render.js";
+import {RenderPosition, render, replace} from "./utils/render.js";
 
 const TASK_COUNT = 20;
 const SHOWING_TASKS_COUNT_ON_START = 8;
@@ -17,11 +17,11 @@ const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const renderTask = (taskListElement, task) => {
   const replaceTaskToEdit = () => {
-    taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+    replace(taskListElement, taskEditComponent.getElement(), taskComponent.getElement());
   };
 
   const replaceEditToTask = () => {
-    taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
+    replace(taskListElement, taskComponent.getElement(), taskEditComponent.getElement());
   };
 
   const onEditButtonClick = () => {
