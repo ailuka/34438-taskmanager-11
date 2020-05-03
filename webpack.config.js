c59838ab5fd67d95a53719e3dfc3b82cfd05964b
Yulia Karpova <karpova.jul@gmail.com>
2020-05-04 02:11:05 +1000

    Подключает moment.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const MomentLocalesPlugin = require(`moment-locales-webpack-plugin`);
 const path = require(`path`);
 
 module.exports = {
@@ -19,5 +20,9 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
-  }
+  },
+  plugins: [
+    // To strip all locales except “en”
+    new MomentLocalesPlugin()
+  ],
 };
