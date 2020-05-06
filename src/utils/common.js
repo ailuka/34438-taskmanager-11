2af00d3dc39d86cfd685f4ec1c5be65f8fa7d78a
Yulia Karpova <karpova.jul@gmail.com>
2020-05-07 00:31:50 +1000

    Создает функции фильтрации и структуру с типами фильтров.

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,3 +7,17 @@ export const formatTime = (date) => {
 export const formatDate = (date) => {
   return moment(date).format(`DD MMMM`);
 };
+
+export const isSameDay = (dateOne, dateTwo) => {
+  const momentOne = moment(dateOne);
+  const momentTwo = moment(dateTwo);
+  return momentOne.diff(momentTwo, `days`) === 0 && dateOne.getDate() === dateTwo.getDate();
+};
+
+export const isOverdueDate = (dueDate, date) => {
+  return dueDate < date && !isSameDay(date, dueDate);
+};
+
+export const isRepeating = (repeatingDays) => {
+  return Object.values(repeatingDays).some(Boolean);
+};
---
+++ src/utils/filter.js
@@ -0,0 +1,53 @@
+import {FilterType} from "../const.js";
+import {isSameDay, isOverdueDate, isRepeating} from "./common.js";
+
+export const getArchiveTasks = (tasks) => {
+  return tasks.filter((task) => task.isArchive);
+};
+
+export const getNotArchiveTasks = (tasks) => {
+  return tasks.filter((task) => !task.isArchive);
+};
+
+export const getFavoriteTasks = (tasks) => {
+  return tasks.filter((task) => task.isFavorite);
+};
+
+export const getOverdueTasks = (tasks, date) => {
+  return tasks.filter((task) => {
+    if (!task.dueDate) {
+      return false;
+    }
+
+    return isOverdueDate(task.dueDate, date);
+  });
+};
+
+export const getRepeatingTasks = (tasks) => {
+  return tasks.filter((task) => isRepeating(task.repeatingDays));
+};
+
+export const getThisDayTasks = (tasks, date) => {
+  return tasks.filter((task) => isSameDay(task.dueDate, date));
+};
+
+export const getTasksByFilter = (tasks, filterType) => {
+  const today = new Date();
+
+  switch (filterType) {
+    case FilterType.All:
+      return getNotArchiveTasks(tasks);
+    case FilterType.ARCHIVE:
+      return getArchiveTasks(tasks);
+    case FilterType.FAVORITES:
+      return getFavoriteTasks(tasks);
+    case FilterType.OVERDUE:
+      return getOverdueTasks(tasks);
+    case FilterType.REPEATING:
+      return getRepeatingTasks();
+    case FilterType.TODAY:
+      return getThisDayTasks(getNotArchiveTasks(tasks), today);
+  }
+
+  return tasks;
+};
