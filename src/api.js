c74ca8c2d6b3f156b98f2615ad964c9032c1e264
Yulia Karpova <karpova.jul@gmail.com>
2020-05-22 00:17:36 +1000

    Создает модуль для отправки на сервер REST запросов.

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import Task from "./models/task.js";
+
+const checkStatus = (response) => {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    throw new Error(`${response.status}: ${response.statusText}`);
+  }
+};
+
+export default class API {
+  constructor(authorization) {
+    this._authorization = authorization;
+  }
+
+  getTasks() {
+    const headers = new Headers();
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks`, {headers})
+      .then(checkStatus)
+      .then((response) => response.json())
+      .then(Task.parseTasks);
+  }
+}
